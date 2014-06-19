15e9080f32d0c71e4e7bad054ebba52b79186b09
Alberto Elias <me@alberto-elias.com>
2014-06-19 15:32:54 +0100

    Removed themes and added horizontal and vertical base styles

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,8 +1,8 @@
 /*global require*/
 
-var ResponsiveNav = require('../../main.js'),
+var oHierarchicalNav = require('../../main.js'),
     navEls = document.querySelectorAll('.o-hierarchical-nav');
 
 for (var c = 0, l = navEls.length; c < l; c++) {
-    new ResponsiveNav(navEls[c]);
+    new oHierarchicalNav(navEls[c]);
 }
--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 /*global require,module*/
-module.exports = require('./src/js/ResponsiveNav');
+function HierarchicalNav(rootEl) {
 
+	function init() {
+		if (rootEl.getAttribute('data-o-hierarchical-nav-orientiation') == 'vertical') {
+			var Nav = require('./src/js/Nav');
+			new Nav(rootEl);
+		} else {
+			var ResponsiveNav = require('./src/js/ResponsiveNav');
+			new ResponsiveNav(rootEl);
+		}
+	}
+
+	init();
+}
+
+module.exports = HierarchicalNav;
